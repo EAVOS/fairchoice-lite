c79994d951518733a533eaf65ffc084da67e54e1
EAVOS <50738262+EAVOS@users.noreply.github.com>
2026-04-27 10:30:54 +0300

    Update app_v2.js

--- app_v2.js
+++ app_v2.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzn2ECL4EQGvx5MsI_MiL_uwQy5l4jFGWYX4zS3q60VOiU0zcDv-dmPVEgf2JT4L6yg9A/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxTjt5SkSNCAlOHHrxVwLSBuNR8yFZ-czeX2m3zkkqYuIpozZQm-zqH7mpoSSXZxGj3lw/exec';
 const webApp = window.Telegram.WebApp;
 webApp.ready();
 webApp.expand();
