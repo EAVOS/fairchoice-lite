e1092a4c36f4b1ad2169cfce3326560d71020ae6
EAVOS <50738262+EAVOS@users.noreply.github.com>
2026-04-28 10:09:49 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Конфигурация приложения
+window.FC_CONFIG = {
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbzhGa9stV7xabSoaujaEGtM6zq57Ndu_b0vxVX_O33gNshk1YuwEWBxE6hrwylZmhtKGQ/exec',
+    BOT_USERNAME: 'FairChoiceBot',
+    WEBAPP_URL: 'https://eavos.github.io/fairchoice-lite/',
+    MAX_RETRIES: 3,
+    RETRY_DELAY: 2000
+};
