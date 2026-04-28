ea3353ab6c3e97a0cbb5fabcfae0efc6c1f0625b
EAVOS <50738262+EAVOS@users.noreply.github.com>
2026-04-28 14:06:39 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Конфигурация приложения
 window.FC_CONFIG = {
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbyAhjb0kPKPI-n11BYQF0QLOhBOjS-VL09nrH5kPEGiamAOlOiSd6fUeGYmg2L0pkGX5w/exec',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbw0MhjjStT5kkZ9HgTnb747JD-iHR7gD5LtqfIzhmT7ekHGj8Otq414qf6F00npDuYx2w/exec',
     BOT_USERNAME: 'FairChoiceBot',
     WEBAPP_URL: 'https://eavos.github.io/fairchoice-lite/',
     MAX_RETRIES: 3,
